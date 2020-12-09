a3e6a4bf81b61c1d486e3cd250394629635a73f1
Dmitry Fedyuk <dfediuk@gmail.com>
2020-12-09 08:27:00 +0600

    https://github.com/innomuebles/m2/issues/10

--- app/code/Alignet/Paymecheckout/Model/Client/Classic/Order.php
+++ app/code/Alignet/Paymecheckout/Model/Client/Classic/Order.php
@@ -7,308 +7,308 @@ namespace Alignet\Paymecheckout\Model\Client\Classic;
 
 class Order implements \Alignet\Paymecheckout\Model\Client\OrderInterface
 {
-    const STATUS_PRE_NEW            = 0;
-    const STATUS_NEW                = 1;
-    const STATUS_CANCELLED          = 2;
-    const STATUS_REJECTED           = 3;
-    const STATUS_PENDING            = 4;
-    const STATUS_WAITING            = 5;
-    const STATUS_REJECTED_CANCELLED = 7;
-    const STATUS_COMPLETED          = 99;
-    const STATUS_ERROR              = 888;
+	const STATUS_PRE_NEW            = 0;
+	const STATUS_NEW                = 1;
+	const STATUS_CANCELLED          = 2;
+	const STATUS_REJECTED           = 3;
+	const STATUS_PENDING            = 4;
+	const STATUS_WAITING            = 5;
+	const STATUS_REJECTED_CANCELLED = 7;
+	const STATUS_COMPLETED          = 99;
+	const STATUS_ERROR              = 888;
 
-    /**
-     * @var string[]
-     */
-    protected $statusDescription = [
-        self::STATUS_PRE_NEW => 'New',
-        self::STATUS_NEW => 'New',
-        self::STATUS_CANCELLED => 'Cancelled',
-        self::STATUS_REJECTED => 'Rejected',
-        self::STATUS_PENDING => 'Pending',
-        self::STATUS_WAITING => 'Waiting for acceptance',
-        self::STATUS_REJECTED_CANCELLED => 'Rejected',
-        self::STATUS_COMPLETED => 'Completed',
-        self::STATUS_ERROR => 'Error'
-    ];
+	/**
+	 * @var string[]
+	 */
+	protected $statusDescription = [
+		self::STATUS_PRE_NEW => 'New',
+		self::STATUS_NEW => 'New',
+		self::STATUS_CANCELLED => 'Cancelled',
+		self::STATUS_REJECTED => 'Rejected',
+		self::STATUS_PENDING => 'Pending',
+		self::STATUS_WAITING => 'Waiting for acceptance',
+		self::STATUS_REJECTED_CANCELLED => 'Rejected',
+		self::STATUS_COMPLETED => 'Completed',
+		self::STATUS_ERROR => 'Error'
+	];
 
-    /**
-     * @var Order\DataValidator
-     */
-    protected $dataValidator;
+	/**
+	 * @var Order\DataValidator
+	 */
+	protected $dataValidator;
 
-    /**
-     * @var Order\DataGetter
-     */
-    protected $dataGetter;
+	/**
+	 * @var Order\DataGetter
+	 */
+	protected $dataGetter;
 
-    /**
-     * @var \Magento\Framework\UrlInterface
-     */
-    protected $urlBuilder;
+	/**
+	 * @var \Magento\Framework\UrlInterface
+	 */
+	protected $urlBuilder;
 
-    /**
-     * @var \Alignet\Paymecheckout\Model\Session
-     */
-    protected $session;
+	/**
+	 * @var \Alignet\Paymecheckout\Model\Session
+	 */
+	protected $session;
 
-    /**
-     * @var \Magento\Framework\App\RequestInterface
-     */
-    protected $request;
+	/**
+	 * @var \Magento\Framework\App\RequestInterface
+	 */
+	protected $request;
 
-    /**
-     * @var \Alignet\Paymecheckout\Logger\Logger
-     */
-    protected $logger;
+	/**
+	 * @var \Alignet\Paymecheckout\Logger\Logger
+	 */
+	protected $logger;
 
-    /**
-     * @var Order\Notification
-     */
-    protected $notificationHelper;
+	/**
+	 * @var Order\Notification
+	 */
+	protected $notificationHelper;
 
-    /**
-     * @var MethodCaller
-     */
-    protected $methodCaller;
+	/**
+	 * @var MethodCaller
+	 */
+	protected $methodCaller;
 
-    /**
-     * @var \Alignet\Paymecheckout\Model\ResourceModel\Transaction
-     */
-    protected $transactionResource;
+	/**
+	 * @var \Alignet\Paymecheckout\Model\ResourceModel\Transaction
+	 */
+	protected $transactionResource;
 
-    /**
-     * @var Order\Processor
-     */
-    protected $orderProcessor;
+	/**
+	 * @var Order\Processor
+	 */
+	protected $orderProcessor;
 
-    /**
-     * @var \Magento\Framework\Controller\Result\RawFactory
-     */
-    protected $rawResultFactory;
+	/**
+	 * @var \Magento\Framework\Controller\Result\RawFactory
+	 */
+	protected $rawResultFactory;
 
-    /**
-     * @param \Magento\Framework\View\Context $context
-     * @param Order\DataValidator $dataValidator
-     * @param Order\DataGetter $dataGetter
-     * @param \Alignet\Paymecheckout\Model\Session $session
-     * @param \Magento\Framework\App\RequestInterface $request
-     * @param \Alignet\Paymecheckout\Logger\Logger $logger
-     * @param Order\Notification $notificationHelper
-     * @param MethodCaller $methodCaller
-     * @param \Alignet\Paymecheckout\Model\ResourceModel\Transaction $transactionResource
-     * @param Order\Processor $orderProcessor
-     * @param \Magento\Framework\Controller\Result\RawFactory $rawResultFactory
-     */
-    function __construct(
-        \Magento\Framework\View\Context $context,
-        Order\DataValidator $dataValidator,
-        Order\DataGetter $dataGetter,
-        \Alignet\Paymecheckout\Model\Session $session,
-        \Magento\Framework\App\RequestInterface $request,
-        \Alignet\Paymecheckout\Logger\Logger $logger,
-        Order\Notification $notificationHelper,
-        MethodCaller $methodCaller,
-        \Alignet\Paymecheckout\Model\ResourceModel\Transaction $transactionResource,
-        Order\Processor $orderProcessor,
-        \Magento\Framework\Controller\Result\RawFactory $rawResultFactory
-    ) {
-        $this->urlBuilder = $context->getUrlBuilder();
-        $this->dataValidator = $dataValidator;
-        $this->dataGetter = $dataGetter;
-        $this->session = $session;
-        $this->request = $request;
-        $this->logger = $logger;
-        $this->notificationHelper = $notificationHelper;
-        $this->methodCaller = $methodCaller;
-        $this->transactionResource = $transactionResource;
-        $this->orderProcessor = $orderProcessor;
-        $this->rawResultFactory = $rawResultFactory;
-    }
+	/**
+	 * @param \Magento\Framework\View\Context $context
+	 * @param Order\DataValidator $dataValidator
+	 * @param Order\DataGetter $dataGetter
+	 * @param \Alignet\Paymecheckout\Model\Session $session
+	 * @param \Magento\Framework\App\RequestInterface $request
+	 * @param \Alignet\Paymecheckout\Logger\Logger $logger
+	 * @param Order\Notification $notificationHelper
+	 * @param MethodCaller $methodCaller
+	 * @param \Alignet\Paymecheckout\Model\ResourceModel\Transaction $transactionResource
+	 * @param Order\Processor $orderProcessor
+	 * @param \Magento\Framework\Controller\Result\RawFactory $rawResultFactory
+	 */
+	function __construct(
+		\Magento\Framework\View\Context $context,
+		Order\DataValidator $dataValidator,
+		Order\DataGetter $dataGetter,
+		\Alignet\Paymecheckout\Model\Session $session,
+		\Magento\Framework\App\RequestInterface $request,
+		\Alignet\Paymecheckout\Logger\Logger $logger,
+		Order\Notification $notificationHelper,
+		MethodCaller $methodCaller,
+		\Alignet\Paymecheckout\Model\ResourceModel\Transaction $transactionResource,
+		Order\Processor $orderProcessor,
+		\Magento\Framework\Controller\Result\RawFactory $rawResultFactory
+	) {
+		$this->urlBuilder = $context->getUrlBuilder();
+		$this->dataValidator = $dataValidator;
+		$this->dataGetter = $dataGetter;
+		$this->session = $session;
+		$this->request = $request;
+		$this->logger = $logger;
+		$this->notificationHelper = $notificationHelper;
+		$this->methodCaller = $methodCaller;
+		$this->transactionResource = $transactionResource;
+		$this->orderProcessor = $orderProcessor;
+		$this->rawResultFactory = $rawResultFactory;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function validateCreate(array $data = [])
-    {
-        return
-            $this->dataValidator->validateEmpty($data) &&
-            $this->dataValidator->validateBasicData($data);
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function validateCreate(array $data = [])
+	{
+		return
+			$this->dataValidator->validateEmpty($data) &&
+			$this->dataValidator->validateBasicData($data);
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function validateRetrieve($paymecheckoutOrderId)
-    {
-        return $this->dataValidator->validateEmpty($paymecheckoutOrderId);
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function validateRetrieve($paymecheckoutOrderId)
+	{
+		return $this->dataValidator->validateEmpty($paymecheckoutOrderId);
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function validateCancel($paymecheckoutOrderId)
-    {
-        return $this->dataValidator->validateEmpty($paymecheckoutOrderId);
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function validateCancel($paymecheckoutOrderId)
+	{
+		return $this->dataValidator->validateEmpty($paymecheckoutOrderId);
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function validateStatusUpdate(array $data = [])
-    {
-        // TODO: Implement validateStatusUpdate() method.
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function validateStatusUpdate(array $data = [])
+	{
+		// TODO: Implement validateStatusUpdate() method.
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function create(array $data)
-    {
-        $this->session->setOrderCreateData($data);
-        
-        return [
-            'orderId' => md5($data['purchaseOperationNumber']),
-            'extOrderId' => $data['purchaseOperationNumber'],
-            'redirectUri' => $this->urlBuilder->getUrl('paymecheckout/classic/form')
-        ];
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function create(array $data)
+	{
+		$this->session->setOrderCreateData($data);
 
-    /**
-     * @inheritDoc
-     */
-    function retrieve($paymecheckoutOrderId)
-    {
-        $posId = $this->dataGetter->getPosId();
-        $ts = $this->dataGetter->getTs();
-        $sig = $this->dataGetter->getSigForOrderRetrieve([
-            'pos_id' => $posId,
-            'purchaseOperationNumber' => $paymecheckoutOrderId,
-            'ts' => $ts
-        ]);
-        $result = $this->methodCaller->call('orderRetrieve', [
-            $posId,
-            $paymecheckoutOrderId,
-            $ts,
-            $sig
-        ]);
-        if ($result) {
-            return [
-                'status' => $result->transStatus,
-                'amount' => $result->transAmount / 100
-            ];
-        }
-        return false;
-    }
+		return [
+			'orderId' => md5($data['purchaseOperationNumber']),
+			'extOrderId' => $data['purchaseOperationNumber'],
+			'redirectUri' => $this->urlBuilder->getUrl('paymecheckout/classic/form')
+		];
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function cancel($paymecheckoutOrderId)
-    {
-        // TODO: Implement cancel() method.
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function retrieve($paymecheckoutOrderId)
+	{
+		$posId = $this->dataGetter->getPosId();
+		$ts = $this->dataGetter->getTs();
+		$sig = $this->dataGetter->getSigForOrderRetrieve([
+			'pos_id' => $posId,
+			'purchaseOperationNumber' => $paymecheckoutOrderId,
+			'ts' => $ts
+		]);
+		$result = $this->methodCaller->call('orderRetrieve', [
+			$posId,
+			$paymecheckoutOrderId,
+			$ts,
+			$sig
+		]);
+		if ($result) {
+			return [
+				'status' => $result->transStatus,
+				'amount' => $result->transAmount / 100
+			];
+		}
+		return false;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function statusUpdate(array $data = [])
-    {
-        // TODO: Implement statusUpdate() method.
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function cancel($paymecheckoutOrderId)
+	{
+		// TODO: Implement cancel() method.
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function consumeNotification(\Magento\Framework\App\Request\Http $request)
-    {
-        $paymecheckoutOrderId = $this->notificationHelper->getPayuplOrderId($request);
-        $orderData = $this->retrieve($paymecheckoutOrderId);
-        if ($orderData) {
-            return [
-                'paymecheckoutOrderId' => md5($paymecheckoutOrderId),
-                'status' => $orderData['status'],
-                'amount' => $orderData['amount']
-            ];
-        }
-        return false;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function statusUpdate(array $data = [])
+	{
+		// TODO: Implement statusUpdate() method.
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function getDataForOrderCreate(\Magento\Sales\Model\Order $order)
-    {
-        return $this->dataGetter->getBasicData($order);
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function consumeNotification(\Magento\Framework\App\Request\Http $request)
+	{
+		$paymecheckoutOrderId = $this->notificationHelper->getPayuplOrderId($request);
+		$orderData = $this->retrieve($paymecheckoutOrderId);
+		if ($orderData) {
+			return [
+				'paymecheckoutOrderId' => md5($paymecheckoutOrderId),
+				'status' => $orderData['status'],
+				'amount' => $orderData['amount']
+			];
+		}
+		return false;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function addSpecialDataToOrder(array $data = [])
-    {
-     
-        return $data;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function getDataForOrderCreate(\Magento\Sales\Model\Order $order)
+	{
+		return $this->dataGetter->getBasicData($order);
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function getNewStatus()
-    {
-        return Order::STATUS_PRE_NEW;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function addSpecialDataToOrder(array $data = [])
+	{
 
-    /**
-     * @inheritDoc
-     */
-    function paymentSuccessCheck()
-    {
-        $errorCode = $this->request->getParam('error');
-        if ($errorCode) {
-            $extOrderId = $this->request->getParam('purchaseOperationNumber');
-            $this->logger->error('Payment error ' . $errorCode . ' for transaction ' . $extOrderId . '.');
-            return false;
-        }
-        return true;
-    }
+		return $data;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function canProcessNotification($paymecheckoutOrderId)
-    {
-        return !in_array(
-            $this->transactionResource->getStatusByPayuplOrderId($paymecheckoutOrderId),
-            [self::STATUS_COMPLETED, self::STATUS_CANCELLED]
-        );
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function getNewStatus()
+	{
+		return Order::STATUS_PRE_NEW;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function processNotification($paymecheckoutOrderId, $status, $amount)
-    {
-        /**
-         * @var $result \Magento\Framework\Controller\Result\Raw
-         */
-        $newest = $this->transactionResource->checkIfNewestByPayuplOrderId($paymecheckoutOrderId);
-        $this->orderProcessor->processStatusChange($paymecheckoutOrderId, $status, $amount, $newest);
-        $result = $this->rawResultFactory->create();
-        $result
-            ->setHttpResponseCode(200)
-            ->setContents('OK');
-        return $result;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function paymentSuccessCheck()
+	{
+		$errorCode = $this->request->getParam('error');
+		if ($errorCode) {
+			$extOrderId = $this->request->getParam('purchaseOperationNumber');
+			$this->logger->error('Payment error ' . $errorCode . ' for transaction ' . $extOrderId . '.');
+			return false;
+		}
+		return true;
+	}
 
-    /**
-     * @inheritDoc
-     */
-    function getStatusDescription($status)
-    {
-        if (isset($this->statusDescription[$status])) {
-            return (string) __($this->statusDescription[$status]);
-        }
-        return false;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	function canProcessNotification($paymecheckoutOrderId)
+	{
+		return !in_array(
+			$this->transactionResource->getStatusByPayuplOrderId($paymecheckoutOrderId),
+			[self::STATUS_COMPLETED, self::STATUS_CANCELLED]
+		);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	function processNotification($paymecheckoutOrderId, $status, $amount)
+	{
+		/**
+		 * @var $result \Magento\Framework\Controller\Result\Raw
+		 */
+		$newest = $this->transactionResource->checkIfNewestByPayuplOrderId($paymecheckoutOrderId);
+		$this->orderProcessor->processStatusChange($paymecheckoutOrderId, $status, $amount, $newest);
+		$result = $this->rawResultFactory->create();
+		$result
+			->setHttpResponseCode(200)
+			->setContents('OK');
+		return $result;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	function getStatusDescription($status)
+	{
+		if (isset($this->statusDescription[$status])) {
+			return (string) __($this->statusDescription[$status]);
+		}
+		return false;
+	}
 }

014a87d6e16386a581e8978757909d244a432d42
Dmitry Fedyuk <dfediuk@gmail.com>
2020-12-09 08:15:05 +0600

    https://github.com/innomuebles/m2/issues/10

--- app/code/Alignet/Paymecheckout/Block/Adminhtml/Order/View/Orderdetail.php
+++ app/code/Alignet/Paymecheckout/Block/Adminhtml/Order/View/Orderdetail.php
@@ -1,31 +1,15 @@
 <?php
 namespace Alignet\Paymecheckout\Block\Adminhtml\Order\View;
-
-class Orderdetail extends \Magento\Backend\Block\Template
-{
-
-	public function __construct(
-		\Magento\Backend\Block\Template\Context $context
-	)
-	{
-		parent::__construct($context);
-	}
-
-	public function getResponse()
-	{
+class Orderdetail extends \Magento\Backend\Block\Template {
+	public function getResponse() {
 		$data = [];
 		$data[0] = '';
 		$data[1] = '';
 		$orderId = $this->getRequest()->getParam('order_id');
-
 		$objectManager =   \Magento\Framework\App\ObjectManager::getInstance();
 		$connection = $objectManager->get('Magento\Framework\App\ResourceConnection')->getConnection('\Magento\Framework\App\ResourceConnection::DEFAULT_CONNECTION');
 		$data[0] = $connection->fetchAll("SELECT * FROM payme_log Where id_order = $orderId");
 		$data[1] = $connection->fetchAll("SELECT * FROM payme_request Where purchaseOperationNumber = $orderId");
-
-
-
 		return $data;
 	}
-
 }

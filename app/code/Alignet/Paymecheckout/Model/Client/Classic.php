1fa9b8bba038e7fb9975be0d6010a782ada27678
Dmitry Fedyuk <dfediuk@gmail.com>
2020-12-09 11:38:06 +0600

    https://github.com/innomuebles/m2/issues/10

--- app/code/Alignet/Paymecheckout/Model/Client/Classic.php
+++ app/code/Alignet/Paymecheckout/Model/Client/Classic.php
@@ -1,23 +1,17 @@
 <?php
-/**
- * @copyright Copyright (c)  2019 Alignet  (https://www.pay-me.com)
- */
-
 namespace Alignet\Paymecheckout\Model\Client;
-
-class Classic extends \Alignet\Paymecheckout\Model\Client
-{
-    /**
-     * @param Classic\Config $configHelper
-     * @param Classic\Order $orderHelper
-     */
-    function __construct(
-        Classic\Config $configHelper,
-        Classic\Order $orderHelper
-    ) {
-        parent::__construct(
-            $configHelper,
-            $orderHelper
-        );
-    }
+final class Classic extends \Alignet\Paymecheckout\Model\Client {
+	/**
+	 * @param Classic\Config $configHelper
+	 * @param Classic\Order $orderHelper
+	 */
+	function __construct(
+		Classic\Config $configHelper,
+		Classic\Order $orderHelper
+	) {
+		parent::__construct(
+			$configHelper,
+			$orderHelper
+		);
+	}
 }
--- app/code/Alignet/Paymecheckout/Model/Client/Rest.php
+++ app/code/Alignet/Paymecheckout/Model/Client/Rest.php
@@ -1,23 +1,17 @@
 <?php
-/**
- * @copyright Copyright (c)  2019 Alignet  (https://www.pay-me.com)
- */
-
 namespace Alignet\Paymecheckout\Model\Client;
-
-class Rest extends \Alignet\Paymecheckout\Model\Client
-{
-    /**
-     * @param Rest\Config $configHelper
-     * @param Rest\Order $orderHelper
-     */
-    function __construct(
-        Rest\Config $configHelper,
-        Rest\Order $orderHelper
-    ) {
-        parent::__construct(
-            $configHelper,
-            $orderHelper
-        );
-    }
+final class Rest extends \Alignet\Paymecheckout\Model\Client {
+	/**
+	 * @param Rest\Config $configHelper
+	 * @param Rest\Order $orderHelper
+	 */
+	function __construct(
+		Rest\Config $configHelper,
+		Rest\Order $orderHelper
+	) {
+		parent::__construct(
+			$configHelper,
+			$orderHelper
+		);
+	}
 }

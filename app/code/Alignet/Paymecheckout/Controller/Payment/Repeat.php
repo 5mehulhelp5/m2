e0ddfbe9e280bb64c82cfba4c68402dd54ac1d2c
Dmitry Fedyuk <dfediuk@gmail.com>
2020-12-09 08:35:53 +0600

    https://github.com/innomuebles/m2/issues/10

--- app/code/Alignet/Paymecheckout/Controller/Payment/Repeat.php
+++ app/code/Alignet/Paymecheckout/Controller/Payment/Repeat.php
@@ -1,41 +1,35 @@
 <?php
-/**
- * @copyright Copyright (c)  2019 Alignet  (https://www.pay-me.com)
- */
-
 namespace Alignet\Paymecheckout\Controller\Payment;
+class Repeat extends \Magento\Framework\App\Action\Action {
+	/**
+	 * @var \Magento\Framework\App\Action\Context
+	 */
+	protected $context;
 
-class Repeat extends \Magento\Framework\App\Action\Action
-{
-    /**
-     * @var \Magento\Framework\App\Action\Context
-     */
-    protected $context;
+	/**
+	 * @var \Alignet\Paymecheckout\Helper\Payment
+	 */
+	protected $paymentHelper;
 
-    /**
-     * @var \Alignet\Paymecheckout\Helper\Payment
-     */
-    protected $paymentHelper;
+	/**
+	 * @var \Alignet\Paymecheckout\Model\Session
+	 */
+	protected $session;
 
-    /**
-     * @var \Alignet\Paymecheckout\Model\Session
-     */
-    protected $session;
+	function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Alignet\Paymecheckout\Helper\Payment $paymentHelper,
+		\Alignet\Paymecheckout\Model\Session $session
+	) {
+		parent::__construct($context);
+		$this->context = $context;
+		$this->paymentHelper = $paymentHelper;
+		$this->session = $session;
+	}
 
-    function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Alignet\Paymecheckout\Helper\Payment $paymentHelper,
-        \Alignet\Paymecheckout\Model\Session $session
-    ) {
-        parent::__construct($context);
-        $this->context = $context;
-        $this->paymentHelper = $paymentHelper;
-        $this->session = $session;
-    }
-
-    function execute()
-    {
-        $resultRedirect = $this->resultRedirectFactory->create();
-        return $resultRedirect;
-    }
+	function execute()
+	{
+		$resultRedirect = $this->resultRedirectFactory->create();
+		return $resultRedirect;
+	}
 }
